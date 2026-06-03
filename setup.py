ad9bd7cc9d2e7dc82c92891995d148e81794f460
vipulthakurji <vipulthakurji7379@gmail.com>
2026-06-03 17:18:02 +0530

    initial folder structure

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setup import setup, find_packages
+
+setup(
+    name="scrapper",
+    version="0.0.1",
+    author="pwskills",
+    author_email="singhvipul3131@gmail.com",
+    packages=find_packages(),
+    install_requires=[],
+)
